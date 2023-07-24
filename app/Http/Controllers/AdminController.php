4471bb940cc396425f0d5a7f85dfbe80a9ef9bc8
gonfreeccs <vicentecamila69@gmail.com>
2023-07-24 18:45:10 +0800

    Commit message

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    //
+    public function loginHandler(Request $request){
+        $fieldtype = filter_var($request->login_id,FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        if($fieldtype == 'email'){
+            $request->validate([
+                'login_id'=>'required|email|exists:admins,email',
+                'password'=>'required|min:5|max:45'
+            ],[
+                'login_id.required'=>'Email or Username is required',
+                'login_id.email'=>'Invalid email address',
+                'login_id.exists'=>'Email does not exists in the system',
+                'password.required'=>'Password is required'
+            ]);
+        }else{
+            $request->validate([
+                'login_id'=>'required|exists:admins,email',
+                'password'=>'required|min:5|max:45'
+            ],[
+                'login_id.required'=>'Email or Username is required',
+                'login_id.exists'=>'Username does not exists in the system',
+                'password.required'=>'Password is required'
+            ]);
+
+        }
+    }
 }
